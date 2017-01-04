9a6eed993d2ebb8380147dac6d1dc9e88ce346c2
Pierre Nagellen <pierre.nagellen@etu.emse.fr>
2017-01-04 13:44:49 +0100

    change afficheDetails

--- js/afficheDetails.js
+++ js/afficheDetails.js
@@ -1,13 +1,4 @@
 //This function show or hide the desription of one of the user parameters
-function afficheDetails(paramName) {
-	if(document.getElementById(paramName + ' button').name === "Show " + paramName){
-		document.getElementById(paramName + ' desc').style.display = "block";
-		document.getElementById(paramName + ' button').value = "Hide description";
-		document.getElementById(paramName + ' button').name = "Hide " + paramName;
-	}
-	else if(document.getElementById(paramName + ' button').name === "Hide " + paramName){
-		document.getElementById(paramName + ' desc').style.display = "none";
-		document.getElementById(paramName + ' button').value = "Show description";
-		document.getElementById(paramName + ' button').name = "Show " + paramName;
-	}
+function afficheDetails(divId) {
+   $("#"+divId).toggle();
 }
