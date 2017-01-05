d6c8cc6e0435a738162e925db25455a816650214
YaYa <yanis.remita@etu.emse.fr>
2017-01-05 21:27:27 +0100

    Ajout de l'api

---
+++ js/meteo.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+// Creation du widget meteo
+window.myWidgetParam = {
+    id: 15,
+    cityid: 0,
+    appid: '91192d3031aace58be1b29211cb36350',
+    containerid: 'openweathermap-widget',
+};
+  // Requete HTTP
+  function loadXMLDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          meteo(this);
+        }
+    };
+    var city = document.getElementById("City").textContent;
+    xhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=91192d3031aace58be1b29211cb36350", true);
+    xhttp.send();
+  }
+
+    // Récupération de l'id de la ville dans le span "City"
+    function meteo(city){
+      var i = 0;
+      var response = "";
+      var cityJson = JSON.parse(city.responseText); // Parse la reponse en Json
+      console.log(cityJson);
+      window.myWidgetParam.cityid = cityJson.id;
+      console.log(window.myWidgetParam);
+    }
+
+  //Appel de la fonction
+  loadXMLDoc();
+
+  // Requête pour avoir le bon temps associé à la ville dans le span "City"
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.src = 'http://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js';
+      console.log(script);
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(script, s);
+});
